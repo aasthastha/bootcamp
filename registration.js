68f19838cb3a0b95d943bec873c2b471040e069c
Aastha Shrestha <as0298791@gmail.com>
2021-12-24 09:37:38 +0545

    gdf

--- registration.js
+++ registration.js
@@ -4,8 +4,8 @@ console.log("This is registration Page");
 const registration = document.getElementById("registration-Form")
 console.log(registration);
 function getSelected(select){
-    let selectyedValues = []
-    for (optionIndex in select) {
+    let selectedValues = []
+    for (optionIndex in select.options) {
         if (select.options[optionIndex].selected){
         selectedValues.push(select.options[optionIndex].value);
     }
@@ -18,7 +18,16 @@ registration.onsubmit = function (event) {
     event.preventDefault();
     console.log("I am triggered when submit is pressed");
     let name = document.getElementById("name").value;
-    let interest = getSelected(document.getElementById("interest"));
+    let interest = getSelected(document.getElementById("interests"));
     let dob = document.getElementById("dob").value;
-    let darkmode = document.getElementById("dark-mode").selected;
+    let darkmode = document.getElementById("dark-mode").checked;
+    const data = {
+        name:name,
+        interest:interest,
+        dob:dob,
+        darkmode:darkmode,
+    };
+    console.log(data);
+
+    
 };
